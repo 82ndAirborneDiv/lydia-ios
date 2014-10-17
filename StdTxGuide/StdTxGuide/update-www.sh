9d0ad2a89fca9a869431a659dfc7aded79192ab0
Greg Ledbetter <gsledbetter@gmail.com>
2014-10-17 14:26:57 -0400

    Updated content.

---
+++ StdTxGuide/StdTxGuide/update-www.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cp ~/informaticslab/lydia-py/mc-gen/gen/conditions/*.html ~/informaticslab/lydia-ios/StdTxGuide/StdTxGuide/content/
+cp ~/informaticslab/lydia-py/mc-gen/gen/condition-content-map.txt ~/informaticslab/lydia-ios/StdTxGuide/StdTxGuide/content/
