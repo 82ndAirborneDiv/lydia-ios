9eca188d45c1a38ff1327bff299dcf9b433f6ee0
Greg Ledbetter <gsledbetter@gmail.com>
2015-02-27 14:36:54 -0500

    Added Full Guidelines tab and HTML page with links to view the STD guide as PDF or HTML.

---
+++ StdTxGuide/StdTxGuide/FullGuidelinesViewController.swift
@@ -0,0 +1,62 @@
+//
+//  FullGuidelinesViewController.swift
+//  StdTxGuide
+//
+//  Created by jtq6 on 2/27/15.
+//  Copyright (c) 2015 jtq6. All rights reserved.
+//
+
+import UIKit
+
+class FullGuidelinesViewController: UIViewController, UIWebViewDelegate {
+
+    @IBOutlet weak var webView: UIWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
+        let url = NSBundle.mainBundle().URLForResource("full", withExtension: "html")
+        let request = NSURLRequest(URL:url!)
+        webView.scalesPageToFit = true
+        webView.delegate = self
+
+        webView.loadRequest(request)
+        
+        self.view.backgroundColor = UIColor(red: 45.0/255.0, green: 88.0/255.0, blue: 167.0/255.0, alpha: 1.0)
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+    
+    func webView(webView: UIWebView!, shouldStartLoadWithRequest request: NSURLRequest!, navigationType: UIWebViewNavigationType) ->Bool  {
+        
+        if (navigationType == UIWebViewNavigationType.LinkClicked)
+            {
+                UIApplication.sharedApplication().openURL(request.URL)
+                return false
+        }
+        
+        return true
+    }
+    
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
