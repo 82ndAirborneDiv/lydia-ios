8293815775e6c35690321df761bd16afc6309705
Greg Ledbetter <gsledbetter@gmail.com>
2015-06-03 16:46:08 -0400

    Add Logging class with global func println that calls Swift.println so that debug print calls can be easily commented out of production code. Point to production SiteCatalyst server.

---
+++ StdTxGuide/StdTxGuide/Logging.swift
@@ -0,0 +1,13 @@
+//
+//  Logging.swift
+//  StdTxGuide
+//
+//  Created by jtq6 on 6/3/15.
+//  Copyright (c) 2015 jtq6. All rights reserved.
+//
+
+import Foundation
+
+func println(object: Any...) {
+    //Swift.println(object)
+}
