c6f4b83a05e72d7f7f27512bec5440eab5d09153
Greg Ledbetter <gsledbetter@gmail.com>
2017-10-19 16:56:55 -0400

    New content with corrected spelling and https instead of http.

--- StdTxGuide/StdTxGuide/update-www.sh
+++ StdTxGuide/StdTxGuide/update-www.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cp ~/informaticslab/lydia-py/mc-gen/gen/conditions/*.html ~/informaticslab/lydia-ios/StdTxGuide/StdTxGuide/content/
-cp ~/informaticslab/lydia-py/mc-gen/gen/condition-content-map.txt ~/informaticslab/lydia-ios/StdTxGuide/StdTxGuide/content/
+cp ~/iiu/lydia-py/mc-gen/gen/conditions/*.html ~/iiu/lydia-ios/StdTxGuide/StdTxGuide/content/
+cp ~/iiu/lydia-py/mc-gen/gen/condition-content-map.txt ~/iiu/lydia-ios/StdTxGuide/StdTxGuide/content/
